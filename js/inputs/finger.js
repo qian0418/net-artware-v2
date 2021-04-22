13d19dd565ccc4c4356e4149f887ea4747b4a3b9
il41 <igilbe1@artic.edu>
2021-04-22 14:41:36 -0500

    initial commit for mediapipe hand tracking stuff

---
+++ js/inputs/finger.js
@@ -0,0 +1,6 @@
+//finger input stuff goes here
+//we are gonna have to figure out how to incorporate
+//mediapipe's own update logic smoothly with the p5 draw
+
+//mediapipe hand tracking example here:
+//https://il41.github.io/netdance/spring.html
