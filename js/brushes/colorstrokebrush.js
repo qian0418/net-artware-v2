12c0de238b283e37a0732a116d13138ff12cedef
Qianx_ <xuqian0418@gmail.com>
2021-05-12 23:19:24 +0800

    Update colorstrokebrush.js

--- js/brushes/colorstrokebrush.js
+++ js/brushes/colorstrokebrush.js
@@ -1,7 +1,7 @@
 var x= Math.random(0,255);
  var y= Math.random(0,255);
  var z= Math.random(0,255);
- var p= 40/Math.pow((windows.input.x-m)^2+(windows.input.y-n)^2,1/4);
+ var p= 40/Math.pow((window.input.x-m)^2+(window.input.y-n)^2,1/4);
 
  window.brushes.colorstrokebrush = {
   name: 'colorstrokebrush',
@@ -11,6 +11,6 @@ var x= Math.random(0,255);
     stroke(x, y, z)
     strokeWeight(p)
 }
-
-var m=windows.input.x;
-var n=windows.input.y;
+}
+var m=window.input.x;
+var n=window.input.y;
