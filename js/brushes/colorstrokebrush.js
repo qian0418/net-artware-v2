134afa7f82946b6ff4cb2968417ada9aec311706
Qianx_ <xuqian0418@gmail.com>
2021-05-12 23:02:04 +0800

    add colorstrokebrush.js

---
+++ js/brushes/colorstrokebrush.js
@@ -0,0 +1,16 @@
+var x=Math.random(0, 360);
+ var y=Math.random(0, 360);
+ var z=Math.random(0,360);
+ var p=40/Math.pow((windows.input.x-m)^2+(windows.input.y-n)^2,1/4);
+
+ window.brushes.colorstrokebrush = {
+  name: 'colorstrokebrush',
+
+  draw: function () {
+    colorMode(RGB)
+    stroke(x, y, z)
+    strokeWeight(p)
+}
+
+var m=windows.input.x;
+var n=windows.input.y;
